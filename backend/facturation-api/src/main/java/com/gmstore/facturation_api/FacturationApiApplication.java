3544b8f1d34d64c90314d209d56c510d3dfa31a6
BelkacemZaid <zaid.belkacem03@gmail.com>
2025-11-03 16:01:13 +0100

    chore: init backend + endpoint /health

---
+++ backend/facturation-api/src/main/java/com/gmstore/facturation_api/FacturationApiApplication.java
@@ -0,0 +1,13 @@
+package com.gmstore.facturation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturationApiApplication.class, args);
+	}
+
+}
